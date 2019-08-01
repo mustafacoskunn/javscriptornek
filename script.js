22971d2ad7ebcf4726d46adcbaa01c62f1eeff79
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-08-01 20:25:49 +0300

    Açıklama satırları

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-const form=document.querySelector('form');
-const input=document.querySelector('#txtTaskName');
+const form=document.querySelector('form'); //form u element olarak seçip degiskene atıyoruz idsini degil formu çektik
+const input=document.querySelector('#txtTaskName'); //direk idsi txtTaskName olanı değişkeinin içine attık
 const btnDeleteAll=document.querySelector('#btnDeleteAll');
 const taskList=document.querySelector('#task-list');
 let items;
-eventListener();
-loadItem();
+eventListener(); // event listerları çalıştırdık
+loadItem();//Local Strogedeki item varmı yokmu kontrol edip var ise ekranda göstericek
 
 
 function eventListener(){
@@ -16,36 +16,36 @@ btnDeleteAll.addEventListener('click',deleteAll);//tum itemleri silme
 
 function loadItem(){
     items=getItemsFromLS(); //LOCAL STROGE
-    items.forEach(function(item){
+    items.forEach(function(item){ //dongu ile geziyor item varmı yok mu
         createItem(item);
     })
     
 }
 //getItemfromLocalStroge
 function getItemsFromLS(){
-    if(localStorage.getItem("items")==null){
+    if(localStorage.getItem("items")==null){ //eger nullssa yani boşsa items diye bi dizi oluştur
         items=[];
     }
     else {
-        items=JSON.parse(localStorage.getItem("items"));
+        items=JSON.parse(localStorage.getItem("items")); //değilse itemsin üzerine items ekle
     }
     return items;
 
 
 }
 function setItemLS(text){
-    items=getItemsFromLS();
-    items.push(text);
-    localStorage.setItem('items',JSON.stringify(items));//String bir ifade olsun diye json.stringify kullanıyoruz
+    items=getItemsFromLS(); //çek
+    items.push(text); //gelen texti pushla
+    localStorage.setItem('items',JSON.stringify(items));//String bir ifade olsun diye json.stringify kullanıyoruz (ekle)
 }
 function createItem(text){
-    const li=document.createElement("li");
-    li.className='list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(text));
-    const a=document.createElement('a');
-    a.className='delete-item float-right';
-    a.setAttribute('href','#');
-    a.innerHTML='<i class="fas fa-times"></i>';
+    const li=document.createElement("li"); //li tagını element olarak ekle
+    li.className='list-group-item list-group-item-secondary'; //ekledigin li tagının classı bu olsun
+    li.appendChild(document.createTextNode(text)); // bunun texti gelen text olsun
+    const a=document.createElement('a'); //yeni element oluştur a tagi
+    a.className='delete-item float-right'; //claası bu olsun
+    a.setAttribute('href','#'); //hrefi boş olsun
+    a.innerHTML='<i class="fas fa-times"></i>';// içinde html kodu kullanmak için innerHTML Kullanmak lazım
 
     li.appendChild(a); //li ye chield ekle a yı
     taskList.appendChild(li);
