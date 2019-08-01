96428cb440ba984c45adab271c6e214975b5714a
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-08-01 20:13:54 +0300

    local stroge kullanıldı.

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const form=document.querySelector('form');
 const input=document.querySelector('#txtTaskName');
 const btnDeleteAll=document.querySelector('#btnDeleteAll');
 const taskList=document.querySelector('#task-list');
-const items=['item1','item2','item3'];
+let items;
 eventListener();
 loadItem();
+
+
 function eventListener(){
 form.addEventListener('submit',addNewItem); //item ekleme
 taskList.addEventListener('click',deleteItem);//item silme
@@ -13,10 +15,28 @@ btnDeleteAll.addEventListener('click',deleteAll);//tum itemleri silme
 }
 
 function loadItem(){
+    items=getItemsFromLS(); //LOCAL STROGE
     items.forEach(function(item){
         createItem(item);
     })
     
+}
+//getItemfromLocalStroge
+function getItemsFromLS(){
+    if(localStorage.getItem("items")==null){
+        items=[];
+    }
+    else {
+        items=JSON.parse(localStorage.getItem("items"));
+    }
+    return items;
+
+
+}
+function setItemLS(text){
+    items=getItemsFromLS();
+    items.push(text);
+    localStorage.setItem('items',JSON.stringify(items));//String bir ifade olsun diye json.stringify kullanıyoruz
 }
 function createItem(text){
     const li=document.createElement("li");
@@ -30,42 +50,64 @@ function createItem(text){
     li.appendChild(a); //li ye chield ekle a yı
     taskList.appendChild(li);
 
+    
+
 }
 
 function addNewItem(e){
     if(input.value.length==0) alert("Boş Bırakılamaz");
     else if(input.value.length>0){
     createItem(input.value);
-    e.preventDefault();
+    setItemLS(input.value); //LOCAL STROGE YE EKLİYORUZ
+   
     
     input.value=""; //input value sıfırla
     }
+    e.preventDefault();
 }
 function deleteItem(e){
 
     if(e.target.className=='fas fa-times'){
         if(confirm('Tüm verileri silmek istiyormusunuz?'))
         e.target.parentElement.parentElement.remove();
+        console.log(e.target.parentElement.parentElement.textContent); // elementdeki texti alıyor
+        deleteItemLS(e.target.parentElement.parentElement.textContent);
     }
     e.preventDefault() //scroll oynamasın diye
 }
 function deleteAll(e){
+    
 
    // taskList.innerHTML=''; direk siler
 
 
    if(confirm('Tüm verileri silmek istiyormusunuz?')){
 
-    taskList.childNodes.forEach(function(item){
-        if(item.nodeType==1) //yani elements ise çünkü text de çekiyor
-        item.remove();
-    });
+  
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }//sıra sıra gezip hepsini sil
+    localStorage.clear();// lsden sil
+
+    
    }
 
 
    e.preventDefault();
 
 
+}
+function deleteItemLS(text){
+    items=getItemsFromLS();
+    items.forEach(function(item,index){
+        if(item==text){ //text ile item aynısı ise Local Strogeden o elamanı sil
+            items.splice(index,1);
+        }
+    });
+    localStorage.setItem('items',JSON.stringify(items));
+
+
+    
 }
 
 
